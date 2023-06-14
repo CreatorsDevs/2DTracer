7bb340aa56d330fcbff5addb8aa638538b1c50eb
CreatorsDevs <creatorsdevs@gmail.com>
2023-06-14 15:59:00 +0530

    Added health system and tweaked UI as per game mechanics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
     [SerializeField] private GameObject playPanelUI;
+    [SerializeField] private GameObject GamePanelUI;
+    [SerializeField] private GameObject pausePanelUI;
+    [SerializeField] private GameObject gameOverPanelUI;
     [SerializeField] private GameObject playerPrefab;
-    private bool isGamePaused = false; // Variable to track if the game has been paused
-    private bool isGameStarted = false; // Variable to track if the game has started
+    [SerializeField] private Image[] hearts;
+    private bool isGamePaused; // Variable to track if the game has been paused
+    public bool IsGameStarted { get; private set; } // Variable to track if the game has started
+    public bool IsGameOver { get; private set; } // Variable to track if the game is over
     public static GameManager instance;
     public GameObject player;
     
@@ -19,24 +24,19 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
      if(instance == null)
-     {
         instance = this;
-        DontDestroyOnLoad(gameObject);
-     }
-     else
-     {
-        Destroy(gameObject);
-     }
     }
     
     void Start()
     {
-        isGameStarted = false;
+        isGamePaused = false;
+        IsGameStarted = false;
+        IsGameOver = false;
     }
 
     void Update()
     {
-        if(isGameStarted) // Only allow pausing if the game has started
+        if(IsGameStarted && !IsGameOver) // Only allow pausing if the game has started
         {
             if(Input.GetKeyDown(KeyCode.Escape))
             {
@@ -53,12 +53,21 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void HandleHealthUI(int currentHealth)
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+                hearts[i].color = i < currentHealth ? Color.white : Color.black;
+        }
+    }
+
     // Functionality for Pausing the game
     private void PauseGame()
     {
         Time.timeScale = 0f;
         isGamePaused = true;
         Debug.Log("Game paused!");
+        pausePanelUI.SetActive(true);
     }
 
     // Functionality for Resuming the game
@@ -67,19 +76,32 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1f;
         isGamePaused = false;
         Debug.Log("Game resumed!");
+        pausePanelUI.SetActive(false);
     }
 
     // Functionality for the Play button in order to start playing the game!
     public void PlayGame()
     {
         playPanelUI.SetActive(false);
-        isGameStarted = true; // Set the game as started when the play button is clicked
+        IsGameStarted = true; // Set the game as started when the play button is clicked
+        IsGameOver = false;
         player = Instantiate(playerPrefab, playerPrefab.transform.position, Quaternion.identity);
+        GamePanelUI.SetActive(true);
     }
 
     // Functionality for the Quit button in order to quit the game!
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver()
+    {
+        IsGameOver = true;
+        gameOverPanelUI.SetActive(true);
     }
 }
