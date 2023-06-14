79c1f468a55132d6c17ddb5a3409b9a20d3524a7
CreatorsDevs <creatorsdevs@gmail.com>
2023-06-14 20:21:38 +0530

    Polished the game

--- Assets/Scripts/Audio/SoundNames.cs
+++ Assets/Scripts/Audio/SoundNames.cs
@@ -1,14 +1,9 @@
 public class SoundNames
 {
     public const string backgroundMusic = "Background Music";
-    public const string buttonSound = "Button";
-    public const string playerDeathSound = "Player Death";
-    public const string playerRespawnSound = "Player Respawn";
-    public const string playerRunSound = "Player Run";
-    public const string playerJumpSound = "Player Jump";
-    public const string newlevelStartSound = "Level Start";
-    public const string levelCompletedSound = "Level Finish";
-    public const string gameOverSound =  "Game Over";
-    public const string keyPickUpSound = "Key Pickup";
     public const string DashSound = "Dash";
+    public const string enemyHurtSound = "EnemyHurt";
+    public const string playerTookDamageSound = "PlayerTookDamage";
+    public const string playerDeathSound = "PlayerDied";
+    public const string buttonSound = "Button";
 }
